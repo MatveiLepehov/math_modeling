b9464040fe76b3c2c2c4586677c81d5eb4fc8b54
Matvei <71832714+MatveiLepehov@users.noreply.github.com>
2020-10-27 17:45:38 +0200

    Add files via upload

--- lab3_5.py
+++ lab3_5.py
@@ -1,4 +1,15 @@
 import numpy as np
-a=np.array([[1,2,3],[4,5,6]])
-print(a[1,:: 1])
-print(a[0,:: 1])
+m=4
+n=3
+a= np.ndarray(shape=(m, n))
+for i in range(m):
+    for j in range(n):
+        if m>0 and n>0:
+            a[i, j] = np.sin(n*i+m*j)
+            if a[i, j] < 0:
+                a[i, j] = 0
+        elif m==0 and n==0:
+            a[i, j] = np.sin(n*(i+1)+m*(j + 1))
+            print(a)
+a[i,j] = a[j,i]
+print(a[i])
