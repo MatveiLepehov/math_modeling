2a0d64020a7e3aa0b3763624468f9a7b6ade6140
Matvei <71832714+MatveiLepehov@users.noreply.github.com>
2020-11-26 17:51:49 +0200

    Add files via upload

---
+++ lab6_5.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+def spiral(b=0.1):
+    f=np.arange(0,8*np.pi,0.1)
+    r=np.e**(b*f)
+    x=r*np.cos(f)
+    y=r*np.sin(f)
+    plt.plot(x,y)
+    plt.show
+spiral()
+    
+    
