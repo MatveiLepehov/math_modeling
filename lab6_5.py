c81ac95d7231b517b8e329b414b9cafb5ce2e921
Matvei <71832714+MatveiLepehov@users.noreply.github.com>
2020-12-01 17:36:13 +0200

    Add files via upload

--- lab6_5.py
+++ lab6_5.py
@@ -8,5 +8,30 @@ def spiral(b=0.1):
     plt.plot(x,y)
     plt.show
 spiral()
-    
-    
+
+def spiral(b=0.1,k=0.1):
+    f=np.arange(0,8*np.pi,0.1)
+    r=k*f
+    x=r*np.cos(f)
+    y=r*np.sin(f)
+    plt.plot(x,y)
+    plt.show
+spiral()
+
+def spiral(k=0.1):
+    f=np.arange(0.1,8*np.pi,0.1)
+    r=k/(0.5*f)
+    x=r*np.cos(f)
+    y=r*np.sin(f)
+    plt.plot(x,y)
+    plt.show
+spiral()
+
+def spiral(k=0.1):
+    f=np.arange(0,8*np.pi,0.1)
+    r=np.sin(k*f)
+    x=r*np.cos(f)
+    y=r*np.sin(f)
+    plt.plot(x,y)
+    plt.show
+spiral()
